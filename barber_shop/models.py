f32456ec6aabc527949ac7e609d547c9617a2a12
bonelli95 <rodrigobonelli95@gmail.com>
2024-05-07 15:16:09 +0100

    creating database

--- barber_shop/models.py
+++ barber_shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.CharField(max_length=100, null= False, blank=False)
+
+    def __str__(self):
+        return f'Fotografia [nome={self.nome}]'
+    
